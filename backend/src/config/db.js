fbf54fd850a22adb59a81c5e6a08b52df8d2c32a
rajaNayak123 <nayakraja151@gmail.com>
2025-09-27 15:25:58 +0530

    complete database connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected Successfully');
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export{
+  connectDB
+}
