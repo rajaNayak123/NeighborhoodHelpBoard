ac737193ed7d1713e5c615c1cc89195a68e1e80f
rajaNayak123 <nayakraja151@gmail.com>
2025-09-27 14:12:52 +0530

    complete the notification model

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      equired: true,
+    },
+    link: {
+      type: String, // Optional link (e.g., to a request or offer)
+    }, 
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export { Notification };
