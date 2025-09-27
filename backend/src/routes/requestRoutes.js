77f944187e756f4015815c06e6cd8bcf9f4911ad
rajaNayak123 <nayakraja151@gmail.com>
2025-09-27 16:22:16 +0530

    complete request route

---
+++ backend/src/routes/requestRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createRequest,
+  getNearbyRequests,
+  getRequestById,
+  updateRequest,
+  deleteRequest,
+} from '../controllers/requestController.js';
+import { protect } from'../middleware/authMiddleware.js';
+import upload from'../config/cloudinary.js';
+
+const router = express.Router();
+
+router.route('/')
+  .post(protect, upload.array('images', 5), createRequest); // upload.array for multiple files
+
+router.get('/nearby', getNearbyRequests);
+
+router.route('/:id')
+  .get(getRequestById)
+  .put(protect, updateRequest)
+  .delete(protect, deleteRequest);
+
+export { router as requestRoutes };
