32b70aa493f5343ef24c58c86bacdc397e1cd357
rajaNayak123 <nayakraja151@gmail.com>
2025-09-27 14:12:19 +0530

    complete the message model

---
+++ backend/src/models/Message.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    request: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Request",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export { Message };
