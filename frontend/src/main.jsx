b402dee5a59c009763d2b794f5a10d98d45f429c
rajaNayak123 <nayakraja151@gmail.com>
2025-10-01 12:10:38 +0530

    import routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
+import { NotificationProvider } from "./context/NotificationContext.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <NotificationProvider>
+            <ChatProvider>
+              <App />
+            </ChatProvider>
+          </NotificationProvider>
+        </SocketProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
