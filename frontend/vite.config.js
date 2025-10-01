e2cc2e39411d025ec24aeb842f5b557ad5bd6e34
rajaNayak123 <nayakraja151@gmail.com>
2025-10-01 12:03:09 +0530

    update port no

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    host: true,
+  },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      process.env.VITE_API_URL || "http://localhost:8000/api"
+    ),
+    
+  },
+});
