d97baefee2cf09e171642ab7d3ce526a71130996
rajaNayak123 <nayakraja151@gmail.com>
2025-10-01 13:31:07 +0530

    update the message service

--- frontend/src/services/messageService.js
+++ frontend/src/services/messageService.js
@@ -1,17 +1,11 @@
-// frontend/src/services/messageService.js
 import api from './api';
 
 const getMessagesForConversation = (requestId, otherUserId) => {
   return api.get(`/messages/${requestId}/${otherUserId}`);
 };
 
-const sendMessage = (receiverId, requestId, content) => {
-  return api.post('/messages', { receiverId, requestId, content });
-};
-
 const messageService = {
   getMessagesForConversation,
-  sendMessage,
 };
 
 export default messageService;
